bbca4d47606733ac8548bd37e5952d3609d7f633
Sadamori Kojaku <skojaku@iu.edu>
2020-10-15 06:46:53 -0400

    change disambiguation script to take json file instead of wos id

--- workflow/disambiguation.py
+++ workflow/disambiguation.py
@@ -1,5 +1,4 @@
-import numpy as np
-import yaml
+import glob
 import pandas as pd
 import os
 import sys
@@ -10,40 +9,25 @@ from leiden_algorithm import LeidenDisambiguationAlgorithm
 
 if __name__ == "__main__":
 
-    ES_CONFIG_FILE = sys.argv[1]
-    INPUT_WOS_FILE = sys.argv[2]
-    WOS_ID_COLUMN_NAME = sys.argv[3]
-    CITATION_DB = sys.argv[4]
-    GENERAL_NAME_LIST_FILE = sys.argv[5]
-    WORKING_DIR = sys.argv[6]
-    OUTPUT = sys.argv[7]
+    INPUT_JSON_DIR = sys.argv[1]
+    CITATION_DB = sys.argv[2]
+    GENERAL_NAME_LIST_FILE = sys.argv[3]
+    WORKING_DIR = sys.argv[4]
+    OUTPUT = sys.argv[5]
 
-    with open(ES_CONFIG_FILE) as f:
-        ES = yaml.safe_load(f)
-        ES_USERNAME = ES["es_username"]
-        ES_PASSWORD = ES["es_password"]
-        ES_ENDPOINT = ES["es_endpoint"]
-
-    wos_ids = (
-        pd.read_csv(INPUT_WOS_FILE)[WOS_ID_COLUMN_NAME]
-        .drop_duplicates()
-        .values.tolist()
-    )
+    json_files = glob.glob(INPUT_JSON_DIR+"/*.json")
 
     general_name_list = pd.read_csv(GENERAL_NAME_LIST_FILE)["first_name"].values
 
     lda = LeidenDisambiguationAlgorithm(
         WORKING_DIR,
-        ES_USERNAME,
-        ES_PASSWORD,
-        ES_ENDPOINT,
         CITATION_DB,
         general_name_list,
         n_jobs=50,
     )
 
     lda.init_working_dir()
-    lda.data_blocking(wos_ids)
+    lda.data_blocking(json_files)
     lda.clustering()
     lda.post_process()
 
